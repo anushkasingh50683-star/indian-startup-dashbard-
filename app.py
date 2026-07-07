7a0c6a862c209025da7c539709a0b88d92ee0fc5
Anushka Singh <anushkasingh50683@gmail.com>
2026-07-07 13:55:24 +0530

    add sidebar

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ df['Investors Name'] = df['Investors Name'].fillna('Undisclosed')
 
 df['Startup Name'] = df['Startup Name'].fillna('Unknown')
 
+st.sidebar.title('Startup Funding Analysis')
+
+option = st.sidebar.selectbox('Select One',['Overall Analysis','Startup','Investor'])
+
+if option == 'Overall Analysis':
+    st.title('Overall Analysis')
+elif option == 'Startup':
+    st.sidebar.selectbox('Select Startup',sorted(df['Startup Name'].unique().tolist()))
+    btn1 = st.sidebar.button('Find Startup Details')
+    st.title('Startup Analysis')
+else:
+    st.sidebar.selectbox(
+    'Select Investor',sorted(df['Investors Name'].unique().tolist()))
+    btn2 = st.sidebar.button('Find Investor Details')
+    st.title('Investor Analysis')
