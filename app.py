350709f35655251fcd0f51f376340eee1d182558
Anushka Singh <anushkasingh50683@gmail.com>
2026-07-08 20:07:32 +0530

    Add Overall Analysis functionalities

--- app.py
+++ app.py
@@ -6,6 +6,49 @@ st.set_page_config(layout='wide',page_title='Startup Analysis')
 
 df = pd.read_csv('startup_cleaned.csv')
 df['date'] = pd.to_datetime(df['date'],errors='coerce')
+df['year'] = df['date'].dt.year
+df['month'] = df['date'].dt.month
+
+def load_overall_analysis():
+    st.title('Overall Analysis')
+
+    # total invested amount
+    total = round(df['amount'].sum())
+
+    # max amount infused in a startup
+    max_funding = df.groupby('startup')['amount'].max().sort_values(ascending = False).head(1).values[0]
+
+    # avg ticket size
+    avg_funding = df.groupby('startup')['amount'].sum().mean()
+
+    # total funded startup
+    num_startups = df['startup'].nunique()
+
+    col1,col2,col3,col4 = st.columns(4)
+
+    with col1:
+        st.metric('Total',str(total) + 'Cr')
+    with col2:
+        st.metric('Max',str(max_funding) + 'Cr')
+    with col3:
+        st.metric('Avg',str(round(avg_funding)) + 'Cr')
+    with col4:
+        st.metric('Funded Startups',num_startups)
+
+    st.header('MOM Graph')
+
+    selected_option = st.selectbox('Select Type',['Total','Count'])
+
+    if selected_option == 'Total':
+        temp_df = df.groupby(['year','month'])['amount'].sum().reset_index()
+    else:
+        temp_df = df.groupby(['year','month'])['amount'].count().reset_index()
+
+    temp_df['x_axis'] = temp_df['month'].astype(str) + '-' + temp_df['year'].astype(str)
+
+    fig3, ax3 = plt.subplots()
+    ax3.plot(temp_df['x_axis'],temp_df['amount'])
+    st.pyplot(fig3)
 
 def load_investor_details(investor):
     st.title(investor)
@@ -52,14 +95,13 @@ def load_investor_details(investor):
         ax3.pie(city_series,labels=city_series.index,autopct="%0.01f%%")
         st.pyplot(fig3)
 
-    df['year'] = df['date'].dt.year
-    
     year_series = df[df['investors'].str.contains(investor,na = False)].groupby('year')['amount'].sum()
 
     st.subheader('Year on Year Investment')
     # display in graph
-    fig3, ax3 = plt.subplots()
-    ax3.plot(year_series.index,year_series.values)
+    fig3, ax3 = plt.subplots(figsize=(5,3))
+    ax3.plot(year_series.index, year_series.values, marker='o')
+    plt.tight_layout()
     st.pyplot(fig3)
 
 
@@ -69,11 +111,13 @@ st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select One',['Overall Analysis','Startup','Investor'])
 
 if option == 'Overall Analysis':
-    st.title('Overall Analysis')
+    load_overall_analysis()
+
 elif option == 'Startup':
     st.sidebar.selectbox('Select Startup',sorted(df['startup'].unique().tolist()))
     btn1 = st.sidebar.button('Find Startup Details')
     st.title('Startup Analysis')
+
 else:
     selected_investor = st.sidebar.selectbox(
     'Select Investor',sorted(set(df['investors'].str.split(',').sum())))
